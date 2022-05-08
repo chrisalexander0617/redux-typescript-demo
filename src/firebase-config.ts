da028ba0c3f5dfbe2b34847d1dc862445e43f3a0
Chris <chrisalexander0617@gmail.com>
2022-05-08 15:40:43 -0400

    Added firebase and typescipt configuration

---
+++ src/firebase-config.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD_gdHREGLkXg3dat_FBc_-sMvLeZ9gksg",
+    authDomain: "redux-firebase-14992.firebaseapp.com",
+    projectId: "redux-firebase-14992",
+    storageBucket: "redux-firebase-14992.appspot.com",
+    messagingSenderId: "631352263032",
+    appId: "1:631352263032:web:7859cc06b77d017975c88d",
+    measurementId: "G-ZG5L71LKBL"
+};
+  
+const app = initializeApp(firebaseConfig)
+
+// Test firebase connection by running ts-node firebase-config.ts
+
+//
+console.log('app', app)
